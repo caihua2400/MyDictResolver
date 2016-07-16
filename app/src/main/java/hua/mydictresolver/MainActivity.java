46a4322bc49eed6c463ddfa4d77357a06292c4f0
cai hua <caihua2300@gmail.com>
2016-07-16 13:11:13 +1000

    a commit for contentResolver to access content Provider written by myself.

--- app/src/main/java/hua/mydictresolver/MainActivity.java
+++ app/src/main/java/hua/mydictresolver/MainActivity.java
@@ -1,13 +1,38 @@
 package hua.mydictresolver;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    ContentResolver contentResolver;
+    Button insert;
+    Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        contentResolver=getContentResolver();
+        insert=(Button) findViewById(R.id.insert);
+        search=(Button) findViewById(R.id.search);
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String word=((EditText)findViewById(R.id.word)).getText().toString();
+                String detail=((EditText)findViewById(R.id.detail)).getText().toString();
+                ContentValues contentValues=new ContentValues();
+                contentValues.put("word",word);
+                contentValues.put("detail",detail);
+                contentResolver.insert();
+
+            }
+        });
+
     }
 }
