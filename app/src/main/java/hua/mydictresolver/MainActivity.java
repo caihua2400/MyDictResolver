1102684cc8ae59466502cd528f2c0a3931e5536e
cai hua <caihua2300@gmail.com>
2016-07-25 20:10:27 +1000

    A successful commit for loader

--- app/src/main/java/hua/mydictresolver/MainActivity.java
+++ app/src/main/java/hua/mydictresolver/MainActivity.java
@@ -8,11 +8,13 @@ import android.net.Uri;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     Button insert;
     Button search;
     String Struri="content://"+"hua.mydictapplication.dictProvider"+"/words";
+    SimpleCursorAdapter simpleCursorAdapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +33,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         contentResolver=getContentResolver();
         insert=(Button) findViewById(R.id.insert);
         search=(Button) findViewById(R.id.search);
-        getLoaderManager().initLoader(URL_LOADER,null,this);
-        insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String word=((EditText)findViewById(R.id.word)).getText().toString();
-                String detail=((EditText)findViewById(R.id.detail)).getText().toString();
-                ContentValues contentValues=new ContentValues();
-                contentValues.put("word",word);
-                contentValues.put("detail",detail);
-                Uri uri=Uri.parse(Struri);
-                contentResolver.insert(uri,contentValues);
-                Toast.makeText(MainActivity.this,"insert success",Toast.LENGTH_LONG).show();
+        getSupportLoaderManager().initLoader(URL_LOADER,null,this);
+        simpleCursorAdapter=new SimpleCursorAdapter(this,R.layout.list_item,null,new String[]{"word","detail"},new int[]{R.id.text1,R.id.text2},0);
+        listView=(ListView) findViewById(R.id.listView);
+        listView.setAdapter(simpleCursorAdapter);
 
-            }
-        });
 
     }
 
@@ -56,11 +50,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Swap the new cursor in.  (The framework will take care of closing the
+        // old cursor once we return.)
+        simpleCursorAdapter.changeCursor(data);
 
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
+        simpleCursorAdapter.swapCursor(null);
 
     }
 }
