e4fed9e8060c5fb1344ac591b964b17574f76c6d
cai hua <caihua2300@gmail.com>
2016-08-03 21:06:18 +1000

    nothing

--- app/src/main/java/hua/mydictresolver/MyAdapter.java
+++ app/src/main/java/hua/mydictresolver/MyAdapter.java
@@ -22,7 +22,7 @@ public class MyAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view= LayoutInflater.from(context).inflate(R.layout.list_item,parent,false);
+        View view= LayoutInflater.from(mContext).inflate(R.layout.list_item,parent,false);
         return view;
     }
 
